eda6892bbf13d507a6e4ac8091e1f17b7d24e6ce
lieluzan3 <lieluzan3@gmail.com>
2024-11-20 20:35:03 +0200

    submissions 3

--- CalcPi.java
+++ CalcPi.java
@@ -1,21 +1,23 @@
 // Computes an approximation of PI.
+
 public class CalcPi {
-	public static void main(String [] args) { 
-	    int numbers= Integer.parseInt(args[0]);
-		double pi=1;
-		double den=3;
-	
-		for (int i = 0; i < numbers; i++) {
-			if(i%2==0){
-				pi-=1/den;
-			}
-			else{
-				pi+=1/den;
-			}
-			den+=2;
-		}
-		System.out.println("pi according to Java: "+Math.PI+"\n"+
-		"pi, approximated:     "+pi*4);
+
+    public static void main(String[] args) {
+        int numbers = 10;
+        double pi = 1;
+        double den = 3;
 		
-	}
+        for (int i = 0; i < numbers-1; i++) {
+			double d = (1.0 / den);
+            if (i % 2 == 0) {
+                pi = pi - d;
+            } else {
+                pi = pi + d;
+            }
+            den += 2;
+        }
+        System.out.println("pi according to Java: " + Math.PI + "\n"
+                + "pi, approximated:     " + pi * 4);
+
+    }
 }
