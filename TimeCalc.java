0605c4c9108f053e3bdfe09b2761f212b318eab2
lieluzan3 <lieluzan3@gmail.com>
2024-11-21 21:10:37 +0200

    submissions 5

--- TimeCalc.java
+++ TimeCalc.java
@@ -5,7 +5,7 @@ public class TimeCalc {
         //time is value of the time we get
         String time =args[0];
         int hours = Integer.parseInt(time.split(":")[0]);
-        int minutes =Integer.parseInt( time.split(":")[1])
+        int minutes =Integer.parseInt( time.split(":")[1]);
         //addminutes is the minutes we add to the time
         int addminutes = Integer.parseInt(args[1]);
         minutes = minutes + addminutes;
